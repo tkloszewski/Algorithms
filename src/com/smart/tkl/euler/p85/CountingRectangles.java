9f61d9ead4f414757a8888677b00a367a464815a
TKLOSZEWSKI <Tomasz.Kloszewski@IGT.com>
2022-03-31 10:09:12 +0200

    - Optimized counting rectangles.

--- src/com/smart/tkl/euler/p85/CountingRectangles.java
+++ src/com/smart/tkl/euler/p85/CountingRectangles.java
@@ -18,49 +18,23 @@ public class CountingRectangles {
 
     public RectGrid findNearestRectArea() {
         int limit = getMaxGridSize(this.countLimit);
-        int[][] countTab = countDP(limit);
 
         int minDiff = Integer.MAX_VALUE;
-        RectGrid foundRectGrid = new RectGrid(1, 1, 1);
-
+        RectGrid foundRectGrid = new RectGrid(1, 1, 1);      
+    
         for(int i = 1; i <= limit; i++) {
-            for(int j = 1; j <= limit; j++) {
-                int diff = Math.abs(countLimit - countTab[i][j]);
+            for(int j = i; j <= limit; j++) {
+                int count = ((i * (i + 1)) / 2) * ((j * (j + 1)) / 2);                
+                int diff = Math.abs(countLimit - count);
                 if(diff < minDiff) {
-                    foundRectGrid = new RectGrid(i, j, countTab[i][j]);
+                    foundRectGrid = new RectGrid(i, j, count);
                     minDiff = diff;
                 }
             }
         }
 
         return foundRectGrid;
-    }
-
-    private static int count(int m, int n) {
-        int result = 0;
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                result += (m - i + 1) * (n - j + 1);
-            }
-        }
-
-        return result;
-    }
-
-    private static int[][] countDP(int limit) {
-        int[][] tab = new int[limit + 1][limit + 1];
-        for(int j = 1; j <= limit; j++) {
-            tab[1][j] = (j * (j + 1)) / 2;
-        }
-
-        for(int j = 1; j <= limit; j++) {
-            for(int i = 2; i <= limit; i++) {
-                tab[i][j] = tab[i - 1][j] + i * (j * (j + 1)) / 2;
-            }
-        }
-
-        return tab;
-    }
+    }      
 
     private static int getMaxGridSize(int countLimit) {
         int delta = 1 + 8 * countLimit;
