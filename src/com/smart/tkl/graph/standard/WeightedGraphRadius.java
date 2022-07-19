66ecf1e1c679559a8f61e7239d722e85dd87a1be
tkloszewski <tkloszewski@wp.pl>
2022-07-19 18:09:42 +0200

    - Implemented weighted graph radius resolver.

---
+++ src/com/smart/tkl/graph/standard/WeightedGraphRadius.java
@@ -0,0 +1,22 @@
+package com.smart.tkl.graph.standard;
+
+import java.math.BigDecimal;
+
+public class WeightedGraphRadius {
+
+    private final BigDecimal radius;
+    private final StandardPath path;
+
+    public WeightedGraphRadius(BigDecimal radius, StandardPath path) {
+        this.radius = radius;
+        this.path = path;
+    }
+
+    public BigDecimal getRadius() {
+        return radius;
+    }
+
+    public StandardPath getPath() {
+        return path;
+    }
+}
