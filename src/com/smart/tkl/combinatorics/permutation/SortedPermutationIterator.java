57f91e37b8350a57dbdad8e23d5adc2095982631
tkloszewski <tkloszewski@wp.pl>
2022-10-15 09:30:58 +0200

    - Implemented sorted permutation iterator with repetition.

--- src/com/smart/tkl/combinatorics/permutation/SortedPermutationIterator.java
+++ src/com/smart/tkl/combinatorics/permutation/SortedPermutationIterator.java
@@ -2,7 +2,6 @@ package com.smart.tkl.combinatorics.permutation;
 
 import com.smart.tkl.SortingUtils;
 import com.smart.tkl.Stack;
-import com.smart.tkl.utils.MathUtils;
 
 import java.util.Arrays;
 import java.util.Iterator;
@@ -11,19 +10,19 @@ public class SortedPermutationIterator implements Iterator<int[]> {
 
 
     private final int[] tab;
-    private final long factorial;
+    private final long totalPermutationsCount;
     private final Stack<StackEntry> stack = new Stack<>();
 
     private int permutationCount = 0;
 
     public SortedPermutationIterator(int[] tab) {
         this.tab = SortingUtils.bubbleSort(tab);
-        factorial = MathUtils.factorial(tab.length);
+        totalPermutationsCount = countTotalPermutations(this.tab);
         stack.push(new StackEntry(0, new int[tab.length], 0, (int)Math.pow(2, tab.length) - 1));
     }
 
     public static void main(String[] args) {
-        Iterator<int[]> iterator = new SortedPermutationIterator(new int[]{1, 2, 3});
+        SortedPermutationIterator iterator = new SortedPermutationIterator(new int[]{1, 2, 2, 2, 3, 3});
         while (iterator.hasNext()) {
             int[] array = iterator.next();
             System.out.println(Arrays.toString(array));
@@ -32,7 +31,7 @@ public class SortedPermutationIterator implements Iterator<int[]> {
 
     @Override
     public boolean hasNext() {
-        return permutationCount < factorial;
+        return permutationCount < totalPermutationsCount;
     }
 
     @Override
@@ -52,17 +51,54 @@ public class SortedPermutationIterator implements Iterator<int[]> {
             }
             else {
                 for (int k = stackEntry.i; k < tab.length; k++) {
-                    int bit = 1 << k;
                     stackEntry.i = k + 1;
-                    if (((mask >> k) & 1) == 1) {
-                        array[pos] = tab[k];
-                        stack.push(new StackEntry(0, array, pos + 1, mask & ~bit));
-                        break;
+                    if(((mask >> k) & 1) == 0) {
+                        continue;
                     }
+                    if(stackEntry.lastValue != null && tab[k] == stackEntry.lastValue) {
+                        continue;
+                    }
+                    int bit = 1 << k;
+                    array[pos] = tab[k];
+                    stackEntry.lastValue = tab[k];
+                    stack.push(new StackEntry(0, array, pos + 1, mask & ~bit));
+                    break;
                 }
             }
         }
         return null;
+    }
+
+    private long countTotalPermutations(int[] sortedTab) {
+        long result = 1;
+        long numerator = sortedTab[0];
+        long denominator = 1;
+        int factorialNumerator = 1;
+
+        for(int i = 1; i < sortedTab.length; i++) {
+            if(sortedTab[i] == sortedTab[i - 1]) {
+                int k = i;
+                int factorialDenominator = 1;
+                while (k < sortedTab.length && sortedTab[k] == sortedTab[k - 1]) {
+                    numerator *= ++factorialNumerator;
+                    denominator *= ++factorialDenominator;
+                    k++;
+                }
+                i = k - 1;
+            }
+            else {
+                numerator *= ++factorialNumerator;
+            }
+            if(denominator != 1 && numerator % denominator == 0) {
+                result *= (double) numerator / denominator;
+                numerator = 1;
+                denominator = 1;
+            }
+        }
+        result *= numerator;
+        result /= denominator;
+
+        return  result;
     }
 
     private static final class StackEntry {
@@ -70,6 +106,7 @@ public class SortedPermutationIterator implements Iterator<int[]> {
         int[] array;
         int pos;
         int freeSlotMask;
+        Integer lastValue = null;
 
         public StackEntry(int i, int[] array, int pos, int freeSlotMask) {
             this.i = i;
