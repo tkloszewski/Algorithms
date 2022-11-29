91ac88bc7a06d3785314ecb8d9dce927c09c3549
tkloszewski <tkloszewski@wp.pl>
2022-11-29 12:19:57 +0100

    - Solved problem 146. Decreased time to 2.5 seconds.

--- src/com/smart/tkl/euler/p146/PrimePattern.java
+++ src/com/smart/tkl/euler/p146/PrimePattern.java
@@ -15,11 +15,13 @@ public class PrimePattern {
 
         List<Long> numbers = new ArrayList<>();
 
-        Set<Long> notAllowedMod11 = Set.of(0L, 2L, 3L, 8L, 9L);
+        Set<Long> notAllowedMod11 = Set.of(2L, 3L, 8L, 9L);
         Set<Long> notAllowedMod13 = Set.of(0L, 2L, 5L, 6L, 7L, 8L, 11L);
         Set<Long> notAllowedMod17 = Set.of(0L, 1L, 2L, 5L, 12L, 15L, 16L);
+        Set<Long> notAllowedMod19 = Set.of(4L, 5L, 14L, 15L);
+        Set<Long> notAllowedMod23 = Set.of(4L, 19L);
 
-        for(long k = 1, n = 10; n < limit; n += 10, k++) {
+        for(long n = 10; n < limit; n += 10) {
             if(n % 3 == 0) {
                continue;
             }
@@ -38,6 +40,14 @@ public class PrimePattern {
             long mod17 = n % 17;
             if(notAllowedMod17.contains(mod17)) {
                continue;
+            }
+            long mod19 = n % 19;
+            if(notAllowedMod19.contains(mod19)) {
+                continue;
+            }
+            long mod23 = n % 23;
+            if(notAllowedMod23.contains(mod23)) {
+                continue;
             }
             long square = n * n;
             if(Primes.isPrime(square + 1) && Primes.isPrime(square + 3)
