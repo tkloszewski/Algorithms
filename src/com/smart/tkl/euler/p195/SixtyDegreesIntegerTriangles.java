9d4cc241800715e8581b6667e892ef59a2e37feb
tkloszewski <tkloszewski@wp.pl>
2023-03-28 08:53:47 +0200

    - Solved problem 195.

---
+++ src/com/smart/tkl/euler/p195/SixtyDegreesIntegerTriangles.java
@@ -0,0 +1,49 @@
+package com.smart.tkl.euler.p195;
+
+import com.smart.tkl.utils.MathUtils;
+
+public class SixtyDegreesIntegerTriangles {
+
+    private final int inscribedRadiusLimit;
+
+    public SixtyDegreesIntegerTriangles(int inscribedRadiusLimit) {
+        this.inscribedRadiusLimit = inscribedRadiusLimit;
+    }
+
+    public static void main(String[] args) {
+        long time1 = System.currentTimeMillis();
+        SixtyDegreesIntegerTriangles sixtyDegreesIntegerTriangles = new SixtyDegreesIntegerTriangles(1053779);
+        long count = sixtyDegreesIntegerTriangles.countTriangles();
+        long time2 = System.currentTimeMillis();
+
+        System.out.println("Count: " + count);
+        System.out.println("Time in ms: " + (time2 - time1));
+    }
+
+    public long countTriangles() {
+        long result = 0;
+
+        int rLimit1 = (int)((double)inscribedRadiusLimit * 6.0 / Math.sqrt(3.0));
+        int rLimit2 = (int)((double)inscribedRadiusLimit * 2.0 / Math.sqrt(3.0));
+
+        for(long n = 1; n * n < rLimit1; n++) {
+            for(long m = n + 3; m * n <= rLimit1; m += 3) {
+                if(MathUtils.GCD(m, n) == 1) {
+                    long k = rLimit1 / (m * n);
+                    result += k;
+                }
+            }
+        }
+
+        for(long n = 1; n * n < rLimit2; n++) {
+            for(long m = n + 1; m * n <= rLimit2; m ++) {
+                if((m - n) % 3 != 0 && MathUtils.GCD(m, n) == 1) {
+                    long k = rLimit2 / (m * n);
+                    result += k;
+                }
+            }
+        }
+
+        return result;
+    }
+}
--- src/com/smart/tkl/euler/p93/ArithmeticExpressions.java
+++ src/com/smart/tkl/euler/p93/ArithmeticExpressions.java
@@ -15,16 +15,19 @@ public class ArithmeticExpressions {
     }
     
     public static void main(String[] args) {
+        long time1 = System.currentTimeMillis();
         ArithmeticExpressions arithmeticExpressions = new ArithmeticExpressions(4);
         ConsecutiveCombination consecutiveCombination = arithmeticExpressions.solve();
+        long time2 = System.currentTimeMillis();
         System.out.println("Found combination: " + consecutiveCombination);
         System.out.println("Sorted form: " + consecutiveCombination.getSortedCombination());
+        System.out.println("Time in ms: " + (time2 - time1));
     }
     
     public ConsecutiveCombination solve() {
         List<int[]> variations = VariationsUtils.generateVariationWithRepetition(new int[]{'+', '-' , '*', '/'}, 3);
         Set<int[]> combinations = CombinatoricsUtils.combinations(Set.of(1, 2, 3, 4, 5, 6 ,7, 8, 9), this.combinationSize);
-        
+
         ConsecutiveCombination longestConsecutiveCombination = null;
         for (int[] combination : combinations) {
             ConsecutiveCombinationEvaluator evaluator = new ConsecutiveCombinationEvaluator(combination, variations);
