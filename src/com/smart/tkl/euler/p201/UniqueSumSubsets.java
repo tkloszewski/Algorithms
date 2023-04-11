dce79243cb01225922772fc79f8be0e970a39b10
tkloszewski <tkloszewski@wp.pl>
2023-04-11 23:04:20 +0200

    - Solved 201 problem.

--- src/com/smart/tkl/euler/p201/UniqueSumSubsets.java
+++ src/com/smart/tkl/euler/p201/UniqueSumSubsets.java
@@ -11,32 +11,36 @@ public class UniqueSumSubsets {
         for(int i = 1; i <= 100; i++) {
             set2.add(i * i);
         }
-        long sum = sum(set2, 50);
+
+        int[] tab = new int[100];
+        for(int i = 1; i <= 100; i++) {
+            tab[i - 1] = i * i;
+        }
+
+        long sum = sum(tab, 50);
         long time2 = System.currentTimeMillis();
 
         System.out.println("Sum2: " + sum);
         System.out.println("Time in ms: " + (time2 - time1));
     }
 
-    private static long sum(List<Integer> set, int k) {
+    private static long sum(int[] set, int k) {
         long result = 0;
 
         int minSum = 0, maxSum = 0;
         for(int i = 0; i < k; i++) {
-            minSum += set.get(i);
+            minSum += set[i];
         }
-        for(int i = set.size() - k ; i < set.size(); i++) {
-            maxSum += set.get(i);
+        for(int i = set.length - k ; i < set.length; i++) {
+            maxSum += set[i];
         }
 
         long[][] ways = new long[maxSum + 1][k + 1];
         ways[0][0] = 1;
 
-        for(int i = 0; i < set.size(); i++) {
+        for(int i = 0; i < set.length; i++) {
             int currentSize = i + 1;
-            int value = set.get(i);
-
-            List<Integer> subSet = set.subList(0, i + 1);
+            int value = set[i];
 
             int[] currentMinSums = new int[k + 1];
             int[] currentMaxSums = new int[k + 1];
@@ -46,10 +50,10 @@ public class UniqueSumSubsets {
             for(int setSize = 1; setSize <= maxSize; setSize++) {
                 int min = 0, max = 0;
                 for(int j = 0; j < setSize; j++) {
-                    min += subSet.get(j);
+                    min += set[j];
                 }
-                for(int j = subSet.size() - setSize ; j < subSet.size(); j++) {
-                    max += subSet.get(j);
+                for(int j = currentSize - setSize ; j < currentSize; j++) {
+                    max += set[j];
                 }
                 currentMinSums[setSize] = min;
                 currentMaxSums[setSize] = max;
