7795c28e603e520307e3cf8cfb032e3fb4518b2c
tkloszewski <tkloszewski@wp.pl>
2023-04-23 11:28:39 +0200

    - Solved problem 231.

--- src/com/smart/tkl/euler/p231/BinomialPrimeFactorization.java
+++ src/com/smart/tkl/euler/p231/BinomialPrimeFactorization.java
@@ -1,7 +1,6 @@
 package com.smart.tkl.euler.p231;
 
-import com.smart.tkl.primes.PrimesSieve;
-import java.util.List;
+import java.util.Arrays;
 
 public class BinomialPrimeFactorization {
 
@@ -18,13 +17,33 @@ public class BinomialPrimeFactorization {
     public static long sumOfTerms(int a, int b) {
         long sum = 0;
         int c = a - b;
-        List<Long> primes = PrimesSieve.generatePrimesUpTo(a);
-        for(long prime : primes) {
-            long pow1 = getPow(a, prime);
-            long pow2 = getPow(b, prime);
-            long pow3 = getPow(c, prime);
-            sum += prime * (pow1 - pow2 - pow3);
+
+        boolean[] sieve = new boolean[a / 2 + 1];
+        Arrays.fill(sieve, true);
+        sieve[0] = false;
+        int primeLimit = (int)Math.sqrt(a);
+        for(int i = 1; i <= primeLimit; i++) {
+            int k = 2 * i + 1;
+            if(sieve[k / 2]) {
+                for (int j = 3; k * j <= a; j += 2) {
+                    int p = k * j;
+                    sieve[p / 2] = false;
+                }
+            }
         }
+
+        for(int i = 0; i < sieve.length; i++) {
+            if(sieve[i] || i == 0) {
+                long prime = i == 0 ? 2L : 2L * i + 1;
+                if(prime <= a) {
+                    long pow1 = getPow(a, prime);
+                    long pow2 = getPow(b, prime);
+                    long pow3 = getPow(c, prime);
+                    sum += prime * (pow1 - pow2 - pow3);
+                }
+            }
+        }
+
         return sum;
     }
 
