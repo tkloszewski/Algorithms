a2591ef6537b4e57875a5b9b996fb1b5696f0009
tkloszewski <tkloszewski@wp.pl>
2023-10-30 22:09:16 +0100

    - Solved problem 176.

--- src/com/smart/tkl/euler/p176/CommonCathetusRightAngledTriangle.java
+++ src/com/smart/tkl/euler/p176/CommonCathetusRightAngledTriangle.java
@@ -3,7 +3,9 @@ package com.smart.tkl.euler.p176;
 import com.smart.tkl.lib.utils.MathUtils;
 import com.smart.tkl.lib.utils.PrimeFactor;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CommonCathetusRightAngledTriangle {
 
@@ -18,36 +20,71 @@ public class CommonCathetusRightAngledTriangle {
     public static long findSmallestCathetus(long n) {
         long m = 2 * n + 1;
         List<PrimeFactor> primeFactors = MathUtils.listPrimeFactors(m);
+        Map<Long, Integer> factorsExpMap = new HashMap<>();
+
         List<Long> factors = new ArrayList<>();
-        for(PrimeFactor primeFactor : primeFactors) {
-            for(long i = 1; i <= primeFactor.getPow(); i++) {
+        for (PrimeFactor primeFactor : primeFactors) {
+            for (long i = 1; i <= primeFactor.getPow(); i++) {
                 factors.add(primeFactor.getFactor());
             }
+            factorsExpMap.put(primeFactor.getFactor(), primeFactor.getPow());
         }
+
         factors.sort((o1, o2) -> -1 * o1.compareTo(o2));
         List<Long> primes = MathUtils.generatePrimeList(factors.size() + 1);
         long evenResult = 1;
-        for(int i = 0; i < factors.size(); i++) {
+
+        long biggestPrimeForEven = primes.get(factors.size() - 1);
+        int lastBigInd = factors.size() - 1;
+
+        for (int i = 0, l = 0; i < factors.size(); i++, l++) {
             long factor = factors.get(i);
-            long prime = primes.get(i);
-            if(prime == 2) {
-               factor = (factor + 1) / 2;
-            }
-            else {
-                factor = (factor - 1) / 2;
-            }
-            for(int k = 0; k < factor; k++) {
+            long prime = primes.get(l);
+            long e = prime == 2 ? (factor + 1) / 2 : (factor - 1) / 2;
+            for (int k = 0; k < e; k++) {
                 evenResult *= prime;
             }
         }
-        long oddResult = 1;
-        for(int i = 0; i < factors.size(); i++) {
-            long factor = (factors.get(i) - 1) / 2;
-            long prime = primes.get(i + 1);
-            for(int k = 0; k < factor; k++) {
-                oddResult *= prime;
+
+        List<PrimeFactor> finalFactors = MathUtils.listPrimeFactors(evenResult);
+        Map<Long, Integer> finalFactorsMap = new HashMap<>();
+        for(PrimeFactor finalFactor : finalFactors) {
+            finalFactorsMap.put(finalFactor.getFactor(), finalFactor.getPow());
+        }
+
+        /*Adjust result by grouping terms.
+        In case of 3^11 we can group as 3^2 * 3^2 * 3 * 3 * 3 * 3 * 3 * 3 * 3 =>
+         2^5 * 3^4 * 5 * 7 * 11 * 13 * 17 * 19 * 23
+         */
+        for (int i = 0, l = 0; i < factors.size(); i++, l++) {
+            long factor = factors.get(i);
+            long prime = primes.get(l);
+            long e = prime == 2 ? (factor + 1) / 2 : (factor - 1) / 2;
+
+            if (factorsExpMap.get(factor) > 1) {
+                long exp = factorsExpMap.get(factor);
+                long newFactor = factor;
+                long adv = 0;
+                for (int j = 2; j <= exp; j++) {
+                    newFactor *= factor;
+                    long pow = prime == 2 ? (newFactor + 1) / 2 : (newFactor - 1) / 2;
+                    long incPow = pow - finalFactorsMap.get(prime);
+                    if ((long) Math.pow(prime, incPow) >= biggestPrimeForEven) {
+                        break;
+                    }
+                    adv++;
+                    e = incPow;
+                }
+                if (adv > 0) {
+                    i += adv;
+                    evenResult = evenResult / biggestPrimeForEven;
+                    evenResult = evenResult * (long)Math.pow(prime, e);
+                    biggestPrimeForEven = primes.get(--lastBigInd);
+                }
             }
         }
-        return Math.min(evenResult, oddResult);
+
+        return evenResult;
     }
 }
+
