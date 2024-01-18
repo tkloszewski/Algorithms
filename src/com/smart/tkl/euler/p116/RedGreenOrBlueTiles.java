b61d0d8d478c0b400fe397cafd611b078710045c
tkloszewski <tkloszewski@wp.pl>
2024-01-18 13:09:42 +0100

    - Version with matrix exponentiation.

--- src/com/smart/tkl/euler/p116/RedGreenOrBlueTiles.java
+++ src/com/smart/tkl/euler/p116/RedGreenOrBlueTiles.java
@@ -1,5 +1,7 @@
 package com.smart.tkl.euler.p116;
 
+import com.smart.tkl.lib.linear.MatrixUtils;
+
 public class RedGreenOrBlueTiles {
 
     private final int length;
@@ -15,13 +17,18 @@ public class RedGreenOrBlueTiles {
         long time2 = System.currentTimeMillis();
         System.out.println("Total ways: " + totalWays);
         System.out.println("Total time in ms: " + (time2 - time1));
+
+        time1 = System.currentTimeMillis();
+        totalWays = redGreenOrBlueTiles.countTotalWaysByMatrixExp();
+        time2 = System.currentTimeMillis();
+        System.out.println("Total ways by matrix exp: " + totalWays);
+        System.out.println("Total time in ms: " + (time2 - time1));
     }
 
     public long countTotalWays() {
         long redTilesWays = countWays(2);
         long greenTilesWays = countWays(3);
         long blueTilesWays = countWays(4);
-
         return redTilesWays + greenTilesWays + blueTilesWays;
     }
 
@@ -46,5 +53,87 @@ public class RedGreenOrBlueTiles {
             waysCount += ways[numberOfColouredTiles][this.length];
         }
         return waysCount;
+    }
+
+    public long countTotalWaysByMatrixExp() {
+        long redCount = countRedWaysByMatrixExp(this.length);
+        long greenCount = countGreenWaysByMatrixExp(this.length);
+        long blueCount = countBlueWaysByMatrixExp(this.length);
+        return redCount + greenCount + blueCount;
+    }
+
+    private static long countRedWaysByMatrixExp(long length) {
+        long[][] initial = new long[][]{
+                {1},
+                {0},
+                {1}
+        };
+
+        if(length <= 2) {
+           return initial[(int)length][0];
+        }
+
+        long[][] matrix = new long[][] {
+                {1, 0, 0},
+                {0, 0, 1},
+                {1, 1, 1}
+        };
+
+        long mod = Long.MAX_VALUE;
+        matrix = MatrixUtils.pow(matrix, length - 2, mod);
+        long[][] result = MatrixUtils.multiply(matrix, initial);
+        return result[2][0];
+    }
+
+    private static long countGreenWaysByMatrixExp(long length) {
+        long[][] initial = new long[][]{
+                {1},
+                {0},
+                {0},
+                {1}
+        };
+
+        if(length <= 3) {
+            return initial[(int)length][0];
+        }
+
+        long[][] matrix = new long[][] {
+                {1, 0, 0, 0},
+                {0, 0, 1, 0},
+                {0, 0, 0, 1},
+                {1, 1, 0, 1}
+        };
+
+        long mod = Long.MAX_VALUE;
+        matrix = MatrixUtils.pow(matrix, length - 3, mod);
+        long[][] result = MatrixUtils.multiply(matrix, initial);
+        return result[3][0];
+    }
+
+    private static long countBlueWaysByMatrixExp(long length) {
+        long[][] initial = new long[][]{
+                {1},
+                {0},
+                {0},
+                {0},
+                {1}
+        };
+
+        if(length <= 4) {
+            return initial[(int)length][0];
+        }
+
+        long[][] matrix = new long[][] {
+                {1, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1},
+                {1, 1, 0, 0, 1}
+        };
+
+        long mod = Long.MAX_VALUE;
+        matrix = MatrixUtils.pow(matrix, length - 4, mod);
+        long[][] result = MatrixUtils.multiply(matrix, initial);
+        return result[4][0];
     }
 }
