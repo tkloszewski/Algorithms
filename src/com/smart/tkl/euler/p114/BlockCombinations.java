61e83193ca72724bad2a5ecedbf563b6d4f9464f
tkloszewski <tkloszewski@wp.pl>
2024-01-19 18:11:51 +0100

    - Some optimized versions for big values.

--- src/com/smart/tkl/euler/p114/BlockCombinations.java
+++ src/com/smart/tkl/euler/p114/BlockCombinations.java
@@ -1,6 +1,7 @@
 package com.smart.tkl.euler.p114;
 
-import java.util.Arrays;
+import com.smart.tkl.lib.combinatorics.CombinatoricsUtils;
+import com.smart.tkl.lib.linear.MatrixUtils;
 
 public class BlockCombinations {
 
@@ -12,12 +13,16 @@ public class BlockCombinations {
     }
 
     public static void main(String[] args) {
+        int length = 50;
         long time1 = System.currentTimeMillis();
         BlockCombinations blockCombinations = new BlockCombinations(50);
         long count = blockCombinations.count();
         long time2 = System.currentTimeMillis();
         System.out.println("Number of combinations: " + count);
         System.out.println("Solution took in ms: " + (time2 - time1));
+
+        count = countUsingMatrixExp(length, 3);
+        System.out.println("Number of combinations by matrix exponentiation: " + count);
     }
 
     public long count() {
@@ -36,4 +41,45 @@ public class BlockCombinations {
 
         return solutions[this.length];
     }
+
+    public static long countUsingMatrixExp(long length, int m) {
+          if(length < m) {
+             return 1;
+          }
+
+          long[][] initial = resolveInitialVector(m);
+          long[][] matrix = resolveMatrix(m);
+
+          long mod = Long.MAX_VALUE;
+          matrix = MatrixUtils.pow(matrix, length - m, mod);
+          long[][] result = MatrixUtils.multiply(matrix, initial, mod);
+
+          long sum = 0;
+          for(int i = 0; i < m + 1; i++) {
+              sum += result[i][0];
+              sum = sum % mod;
+          }
+          return sum;
+    }
+
+    private static long[][] resolveInitialVector(int m) {
+        long[][] initial = new long[m + 1][1];
+        initial[0][0] = 1;
+        initial[m][0] = 1;
+        return initial;
+    }
+
+    private static long[][] resolveMatrix(int m) {
+        long[][] matrix = new long[m + 1][m + 1];
+        matrix[0][0] = 1;
+        matrix[0][m] = 1;
+        matrix[m][m - 1] = 1;
+        matrix[m][m] = 1;
+        for(int i = 1, j = 0; i <= m - 1; i++, j++) {
+            matrix[i][j] = 1;
+        }
+        return matrix;
+    }
+
+
 }
--- src/com/smart/tkl/euler/p115/BlockCombinations2.java
+++ src/com/smart/tkl/euler/p115/BlockCombinations2.java
@@ -1,11 +1,12 @@
 package com.smart.tkl.euler.p115;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class BlockCombinations2 {
 
-   private final int minLength;
-   private final long valueThreshold;
+    private final int minLength;
+    private final long valueThreshold;
 
     public BlockCombinations2(int minLength, long valueThreshold) {
         this.minLength = minLength;
@@ -13,12 +14,27 @@ public class BlockCombinations2 {
     }
 
     public static void main(String[] args) {
+        long limit = (long)Math.pow(10, 6);
         long time1 = System.currentTimeMillis();
-        BlockCombinations2 blockCombinations = new BlockCombinations2(50, 1000000);
-        int rowLength = blockCombinations.findRowLength();
+        BlockCombinations2 blockCombinations = new BlockCombinations2(50, limit);
+        long rowLength = blockCombinations.findRowLength();
         long time2 = System.currentTimeMillis();
         System.out.println("Row length: " + rowLength);
         System.out.println("Solution found in ms: " + (time2 - time1));
+
+       // long limit1 = (long)Math.pow(10, 18);
+        long limit1 = 1;
+        long m = 10000000;
+        rowLength = findMaxRowLength(m, limit1);
+        System.out.println("Row length: " + rowLength);
+
+       // m = (long)Math.pow(10, 18);
+        rowLength = search(m, limit1);
+        System.out.println("Row length binary search: " + rowLength);
+
+
+
+        /*1000000001414213561*/
     }
 
     public int findRowLength() {
@@ -38,7 +54,7 @@ public class BlockCombinations2 {
                 }
                 solutions[pos]++;
                 if(solutions[pos] > valueThreshold) {
-                   return pos;
+                    return pos;
                 }
             }
 
@@ -47,4 +63,71 @@ public class BlockCombinations2 {
             solutions = Arrays.copyOf(solutions, length + 1);
         }
     }
+
+    public static long findMaxRowLength(long m, long countLimit) {
+        long n = m;
+        while (true) {
+            boolean exceeded = test(n, m, countLimit);
+            if(exceeded) {
+                return n;
+            }
+            n++;
+        }
+    }
+
+    public static long search(long m, long countLimit) {
+        return search(m, m, Long.MAX_VALUE, countLimit);
+    }
+
+    private static long search(long m, long left, long right, long countLimit) {
+        if(left == right) {
+            return left;
+        }
+        long middle = left + (right - left) / 2;
+        boolean exceeded = test(middle, m, countLimit);
+        if(exceeded) {
+            return search(m, left, middle, countLimit);
+        }
+        else {
+            return search(m, middle + 1, right, countLimit);
+        }
+    }
+
+    private static boolean test(long n, long m, long limit) {
+        long count = 1;
+        long max = (n + 1) / (m + 1);
+
+        for(int b = 0; b <= max; b++) {
+            long partCount = combinations(1 + b - b * m + n, 1 - b - b * m + n, limit);
+            if(partCount > limit) {
+                return true;
+            }
+            count += partCount;
+            if(count > limit) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static long combinations(long n, long k, long limit) {
+        if(k > n - k) {
+            k = n - k;
+        }
+
+        BigInteger result = BigInteger.ONE;
+        BigInteger bgLimit = BigInteger.valueOf(limit);
+
+        for(long i = 0; i < k; i++) {
+            result = result.multiply(BigInteger.valueOf(n - i));
+            result = result.divide(BigInteger.valueOf(i + 1));
+            if(result.compareTo(bgLimit) > 0) {
+                return limit + 1;
+            }
+        }
+
+        return result.longValue();
+    }
+
 }
