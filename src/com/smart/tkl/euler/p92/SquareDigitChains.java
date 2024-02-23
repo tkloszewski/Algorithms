b72aa603ee0b71719f0ad6624da8f182b643b66b
tkloszewski <tkloszewski@wp.pl>
2024-02-23 13:54:09 +0100

    - Implemented more efficient algorithm for Square Digit Chains problem.

--- src/com/smart/tkl/euler/p92/SquareDigitChains.java
+++ src/com/smart/tkl/euler/p92/SquareDigitChains.java
@@ -1,86 +1,115 @@
 package com.smart.tkl.euler.p92;
 
+import com.smart.tkl.lib.utils.MathUtils;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class SquareDigitChains {
 
-    private final int limit;
     private final int digitLength;
+    private final long mod;
 
     private boolean[] arrivalsAt89;
-    private int[] factorials;
+    private BigInteger[] factorials;
 
-    public SquareDigitChains(int limit) {
-        this.limit = limit;
-        this.digitLength = (int)Math.log10(limit);
-    }
-
-    public SquareDigitChains(int limit, int digitLength) {
-        this.limit = limit;
+    public SquareDigitChains(int digitLength, long mod) {
         this.digitLength = digitLength;
+        this.mod = mod;
     }
 
     public static void main(String[] args) {
+        long mod = (long)Math.pow(10, 9) + 7;
+        int k = 200;
         long time1 = System.currentTimeMillis();
-        SquareDigitChains squareDigitChains = new SquareDigitChains(10000000);
-        int fastCountResult = squareDigitChains.fastCount();
+        SquareDigitChains squareDigitChains = new SquareDigitChains(k, mod);
+        long fastCountResult = squareDigitChains.fastCount2();
         long time2 = System.currentTimeMillis();
         System.out.printf("Arrives at 89 fast count result %d. Time in ms: %d%n",
                 fastCountResult, (time2 - time1));
     }
 
-    public int slowCount() {
-        int count = 0;
-        for(int i = 1; i < limit; i++) {
-            int arrivesAt = i;
-            while (arrivesAt != 89 && arrivesAt != 1) {
-                arrivesAt = getSquareDigits(arrivesAt);
-            }
-            if(arrivesAt == 89) {
-                count++;
-            }
-
+    public long fastCount2() {
+        long[] sumsCount = new long[digitLength * 89 + 1];
+        for(int digit = 0; digit <= 9; digit++) {
+            sumsCount[digit * digit]++;
         }
+
+        for(int length = 2; length <= digitLength; length++) {
+            for(int sum = 81 * length; sum > 0; sum--) {
+                for(int digit = 1; digit <= 9; digit++) {
+                    int square = digit * digit;
+                    if(square > sum) {
+                       break;
+                    }
+                    sumsCount[sum] += sumsCount[sum - square];
+                    sumsCount[sum] = sumsCount[sum] % mod;
+                }
+            }
+        }
+
+        long count = 0;
+
+        for(int sum = 1; sum <= digitLength * 89; sum++) {
+            if(isArrival89(sum)) {
+               count += sumsCount[sum];
+               count = count % mod;
+            }
+        }
+
         return count;
     }
 
-    public int fastCount() {
-        factorials = new int[digitLength + 1];
-        factorials[0] = 1;
+    public long fastCount() {
+        factorials = new BigInteger[digitLength + 1];
+        factorials[0] = BigInteger.ONE;
         for(int i = 1; i <= digitLength; i++) {
-            factorials[i] = factorials[i - 1] * i;
+            factorials[i] = factorials[i - 1].multiply(BigInteger.valueOf(i));
         }
+        List<Integer> arrivalsValues = new ArrayList<>();
         arrivalsAt89 = new boolean[digitLength * 89 + 1];
         for(int i = 1; i < arrivalsAt89.length; i++) {
             arrivalsAt89[i] = isArrival89(i);
+            if(arrivalsAt89[i]) {
+               arrivalsValues.add(i);
+            }
         }
+        System.out.println(arrivalsValues.size());
 
         int[] combination = new int[10];
         return countCombinations(0, digitLength, combination);
     }
 
-    private int countCombinations(int idx, int left, int[] combination) {
+    private long countCombinations(int idx, int left, int[] combination) {
         if(idx + 1 == combination.length) {
             combination[idx] = left;
             return countPermutations(combination);
         }
-        int result = 0;
+        long result = 0;
         for(int i = 0; i <= left; i++) {
             combination[idx] = i;
             result += countCombinations(idx + 1, left - i, combination);
+            result = result % mod;
         }
         return result;
     }
 
-    private int countPermutations(int[] combination) {
+    private long countPermutations(int[] combination) {
         int arrivalAt = 0;
         for(int i = 0; i < combination.length; i++) {
             arrivalAt += combination[i] * i * i;
         }
         if(arrivalsAt89[arrivalAt]) {
-           int denominator = 1;
+           BigInteger denominator = BigInteger.ONE;
            for(int freq : combination) {
-               denominator *= factorials[freq];
+               if (freq != 0) {
+                   denominator = denominator.multiply(factorials[freq]);
+               }
            }
-           return factorials[digitLength] / denominator;
+           return factorials[digitLength].divide(denominator).mod(BigInteger.valueOf(mod)).longValue();
         }
         return 0;
     }
