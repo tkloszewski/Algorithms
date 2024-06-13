a60c79560192deff888f726cba8975c61f298023
tkloszewski <tkloszewski@wp.pl>
2024-06-13 14:50:58 +0200

    - Implemented Hackerrank optimized version of problem 121.

--- src/com/smart/tkl/euler/p121/DiscGamePrizeFund.java
+++ src/com/smart/tkl/euler/p121/DiscGamePrizeFund.java
@@ -4,55 +4,86 @@ import com.smart.tkl.lib.combinatorics.CombinatoricsUtils;
 import com.smart.tkl.lib.utils.MathUtils;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.math.MathContext;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class DiscGamePrizeFund {
 
     private final int turns;
+    private final BigInteger[][] numeratorMemo;
 
     public DiscGamePrizeFund(int turns) {
         this.turns = turns;
+        this.numeratorMemo = new BigInteger[turns + 1][turns + 1];
     }
 
     public static void main(String[] args) {
+        int maxTurns = 15;
         long time1 = System.currentTimeMillis();
-        DiscGamePrizeFund discGamePrizeFund = new DiscGamePrizeFund(15);
-        int maxPrize = discGamePrizeFund.resolveMaxPrize();
+        DiscGamePrizeFund discGamePrizeFund = new DiscGamePrizeFund(maxTurns);
+        BigInteger maxPrize = discGamePrizeFund.resolveMaxPrize();
         long time2 = System.currentTimeMillis();
         System.out.println("Max prize: " + maxPrize);
         System.out.println("Solution found in ms: " + (time2 - time1));
     }
 
-    public int resolveMaxPrize() {
-        long winingProbabilityDenominator = MathUtils.factorial(turns + 1);
-        long winingProbabilityNumerator = 1;
+    public BigInteger resolveMaxPrize() {
+        return resolveMaxPrize(this.turns);
+    }
 
-        Set<Integer> allTurns = new HashSet<>();
-        for(int i = 1; i <= turns; i++) {
-            allTurns.add(i);
+    public BigInteger resolveMaxPrize(int turns) {
+        BigInteger numerator = calcNumerator(turns);
+        BigInteger denominator = calcDenominator(turns);
+        return denominator.divide(numerator);
+    }
+
+    private BigInteger calcDenominator(int turns) {
+        BigInteger denominator = BigInteger.ONE;
+        for(int i = 2; i <= turns + 1; i++) {
+            denominator = denominator.multiply(BigInteger.valueOf(i));
         }
+        return denominator;
+    }
 
+    private BigInteger calcNumerator(int turns) {
+        BigInteger numerator = BigInteger.ONE;
         int maxRedDisksChosen = turns % 2 == 0 ? turns / 2 - 1 : turns / 2;
-        for(int redDiskCount = 1; redDiskCount <= maxRedDisksChosen; redDiskCount++) {
-            Set<int[]> winingCombinations = CombinatoricsUtils.combinations(allTurns, redDiskCount);
-            for(int[] winingCombination : winingCombinations) {
-                int numeratorPart = 1;
-                for(int winingPosition : winingCombination) {
-                    numeratorPart *= winingPosition;
-                }
-                winingProbabilityNumerator += numeratorPart;
-            }
+        for(int redDisks = 1; redDisks <= maxRedDisksChosen; redDisks++) {
+            BigInteger combinationNumerator = calcNumerator(turns, redDisks);
+            numerator = numerator.add(combinationNumerator);
+        }
+        return numerator;
+    }
+
+    private BigInteger calcNumerator(int n, int k) {
+        BigInteger result = numeratorMemo[n][k];
+        if(result != null) {
+           return result;
         }
 
-        return calculateMaxPrize(winingProbabilityDenominator, winingProbabilityNumerator);
-    }
+        if(n == k) {
+           result = BigInteger.ONE;
+           for(int i = 2; i <= n; i++) {
+               result = result.multiply(BigInteger.valueOf(i));
+           }
+        }
+        else if(k == 0) {
+           result = BigInteger.ONE;
+        }
+        else if(k == 1) {
+           BigInteger N = BigInteger.valueOf(n);
+           result = N.multiply(BigInteger.ONE.add(N)).divide(BigInteger.TWO);
+        }
+        else {
+          BigInteger r1 = calcNumerator(n - 1, k- 1);
+          BigInteger r2 = calcNumerator(n - 1, k);
+          result = BigInteger.valueOf(n).multiply(r1).add(r2);
+        }
 
-    private int calculateMaxPrize(long winingProbabilityDenominator, long winingProbabilityNumerator) {
-        long losingProbabilityNumerator = winingProbabilityDenominator - winingProbabilityNumerator;
-        return (int)Math.ceil(BigDecimal.valueOf(losingProbabilityNumerator)
-                .divide(BigDecimal.valueOf(winingProbabilityNumerator), MathContext.DECIMAL128).doubleValue());
+        numeratorMemo[n][k] = result;
+        return result;
     }
-
 }
